e89aa444267c3381ff72c58ccd989a0fd6722fe0
Vladimir <vladimir.voblikov@t-systems.com>
2021-01-05 23:23:08 +0300

    Initial commit

---
+++ src/main/java/com/vmt/streamtest/JaxrsApplication.java
@@ -0,0 +1,9 @@
+package com.vmt.streamtest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JaxrsApplication extends Application {
+
+}
